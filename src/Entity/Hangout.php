472553c6c1d4dc7add9c45f0e1b8b9a507482193
vivien <spamoremas@gmail.com>
2023-03-27 12:01:23 +0200

    made hangout && state

---
+++ src/Entity/Hangout.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HangoutRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HangoutRepository::class)]
+class Hangout
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startTimestamp = null;
+
+    #[ORM\Column]
+    private ?\DateInterval $duration = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $lastRegisterDate = null;
+
+    #[ORM\Column]
+    private ?int $maxSlots = null;
+
+    #[ORM\Column(length: 3000)]
+    private ?string $informations = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartTimestamp(): ?\DateTimeInterface
+    {
+        return $this->startTimestamp;
+    }
+
+    public function setStartTimestamp(\DateTimeInterface $startTimestamp): self
+    {
+        $this->startTimestamp = $startTimestamp;
+
+        return $this;
+    }
+
+    public function getDuration(): ?\DateInterval
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(\DateInterval $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getLastRegisterDate(): ?\DateTimeInterface
+    {
+        return $this->lastRegisterDate;
+    }
+
+    public function setLastRegisterDate(\DateTimeInterface $lastRegisterDate): self
+    {
+        $this->lastRegisterDate = $lastRegisterDate;
+
+        return $this;
+    }
+
+    public function getMaxSlots(): ?int
+    {
+        return $this->maxSlots;
+    }
+
+    public function setMaxSlots(int $maxSlots): self
+    {
+        $this->maxSlots = $maxSlots;
+
+        return $this;
+    }
+
+    public function getInformations(): ?string
+    {
+        return $this->informations;
+    }
+
+    public function setInformations(string $informations): self
+    {
+        $this->informations = $informations;
+
+        return $this;
+    }
+}
