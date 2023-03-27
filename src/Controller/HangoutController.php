0321551b9526ba964b52a36adcde2bad6134a184
anegro2022 <alexandre.fj.negro@gmail.com>
2023-03-27 16:28:30 +0200

    base nav list

--- src/Controller/HangoutController.php
+++ src/Controller/HangoutController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\HangoutRepository;
+use ContainerC63iavh\getHangoutRepositoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,5 +19,29 @@ class HangoutController extends AbstractController
         return $this->render('hangout/list.html.twig', [
             'hangouts' => $hangouts
         ]);
+    }
+
+    #[Route('details/{id}', name: 'details')]
+    public function details(int $id,
+                            getHangoutRepositoryService $hr): Response
+    {
+
+        $hangout = $hr->find($id);
+
+        return $this->render('hangout/details.html.twig', [
+            'hangout' => $hangout
+        ]);
+    }
+
+    #[Route('/goingTo', name: 'goingTo')]
+    public function goingTo() {
+
+        return $this->redirectToRoute('hangout_list', []);
+    }
+
+    #[Route('/notGoingAnymore', name: 'notGoingAnymore')]
+    public function notGoingAnymore () {
+
+        return $this->redirectToRoute('hangout_list', []);
     }
 }
