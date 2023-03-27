c87d80ffa6a07047c543ac636e4636e11adf95f3
vivien <spamoremas@gmail.com>
2023-03-27 13:46:25 +0200

    State class: wording constraints

--- src/Entity/State.php
+++ src/Entity/State.php
@@ -4,16 +4,20 @@ namespace App\Entity;
 
 use App\Repository\StateRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: StateRepository::class)]
 class State
 {
+    private const wordingState = ['created', 'reg_open', 'reg_closed', 'in_progress', 'done', 'canceled'];
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 15)]
+    #[Assert\Choice(choices: State::wordingState, message: 'select a valid state')]
     private ?string $wording = null;
 
     public function getId(): ?int
