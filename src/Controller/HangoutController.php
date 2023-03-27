e570ef7cf4983fbdcbbe149615eccf09d2417b49
smbongui2022 <sidney.mbongui2022@campus-eni.fr>
2023-03-27 16:32:04 +0200

    hangout

--- src/Controller/HangoutController.php
+++ src/Controller/HangoutController.php
@@ -2,21 +2,18 @@
 
 namespace App\Controller;
 
-use App\Repository\HangoutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-#[Route('/hangout', name: 'hangout_')]
+#[Route('hangout', name:'hangout_')]
 class HangoutController extends AbstractController
 {
-    #[Route('/list', name: 'list')]
-    public function index(HangoutRepository $hr): Response
+    #[Route('/create', name: 'create')]
+    public function createHangout(): Response
     {
-        $hangouts = $hr->findAll();
 
-        return $this->render('hangout/list.html.twig', [
-            'hangouts' => $hangouts
+        return $this->render('hangout/index.html.twig', [
+            'controller_name' => 'HangoutController',
         ]);
     }
 }
