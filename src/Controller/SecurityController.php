250a02f00a63a08a13c375fba7253e97dfc41378
anegro2022 <alexandre.fj.negro@gmail.com>
2023-03-28 11:42:28 +0200

    css fix details add

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,7 +12,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        var_dump($this->getUser());
          if ($this->getUser()) {
              return $this->redirectToRoute('home');
          }
