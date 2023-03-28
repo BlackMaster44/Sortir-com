773de50bf3d6f62c40a1d0672e9160e249aeb37f
smbongui2022 <sidney.mbongui2022@campus-eni.fr>
2023-03-28 10:03:49 +0200

    hangout

--- src/Controller/HangoutController.php
+++ src/Controller/HangoutController.php
@@ -2,24 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Hangout;
+
+use App\Form\CreateHangoutType;
+
 use App\Repository\HangoutRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('hangout', name:'hangout_')]
 class HangoutController extends AbstractController
 {
     #[Route('/create', name: 'create')]
-    public function createHangout(): Response
+    public function createHangout(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $hangout = new Hangout();
+        $hangoutForm = $this->createForm(CreateHangoutType::class, $hangout);
+        $hangoutForm->handleRequest($request);
 
-        return $this->render('hangout/create.html.twig', []);
+        if($hangoutForm->isSubmitted() && $hangoutForm->isValid()) {
+
+            $entityManager->persist($hangout);
+            $entityManager->flush();
+
+
+            $this->addFlash('success', 'Hangout added!!!');
+
+            return $this->redirectToRoute('hangout_list');
+        }
+        return $this->render('hangout/create.html.twig',[
+            'hangoutForm'=>$hangoutForm,
+        ]);
     }
-
     #[Route('/list', name: 'list')]
-    public function list(HangoutRepository $hr): Response
-    {
+    public function list(HangoutRepository $hr) {
         $hangouts = $hr->findAll();
 
         return $this->render('hangout/list.html.twig', [
@@ -27,27 +46,28 @@ class HangoutController extends AbstractController
         ]);
     }
 
-    #[Route('details/{id}', name: 'details')]
+#[Route('details/{id}', name: 'details')]
     public function details(int $id, HangoutRepository $hr): Response
-    {
+{
 
-        $hangout = $hr->find($id);
+    $hangout = $hr->find($id);
 
-        return $this->render('hangout/details.html.twig', [
-            'hangout' => $hangout
-        ]);
-    }
+    return $this->render('hangout/details.html.twig', [
+        'hangout' => $hangout
+    ]);
+}
 
     #[Route('/goingTo', name: 'goingTo')]
     public function goingTo() {
 
-        return $this->redirectToRoute('hangout_list', []);
-    }
+    return $this->redirectToRoute('hangout_list', []);
+}
 
     #[Route('/notGoingAnymore', name: 'notGoingAnymore')]
-    public function notGoingAnymore (): RedirectResponse
-    {
+    public function notGoingAnymore () {
 
-        return $this->redirectToRoute('hangout_list', []);
-    }
+    return $this->redirectToRoute('hangout_list', []);
+}
+
+
 }
