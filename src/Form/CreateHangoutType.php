9b2596cfd14bfeb153a86a8bdb27b7bcc44a3fee
vivien <spamoremas@gmail.com>
2023-03-30 16:09:21 +0200

    fixed create form

--- src/Form/CreateHangoutType.php
+++ src/Form/CreateHangoutType.php
@@ -2,22 +2,16 @@
 
 namespace App\Form;
 
-use App\Entity\City;
 use App\Entity\Hangout;
 use App\Entity\Place;
-use App\Entity\Site;
-use App\Repository\CityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CreateHangoutType extends AbstractType
@@ -27,7 +21,7 @@ class CreateHangoutType extends AbstractType
 
         $builder
             ->add('name')
-            ->add('startTimestamp',DateType::class, [
+            ->add('startTimestamp',DateTimeType::class, [
                 'widget' => 'single_text'
             ])
             ->add('lastRegisterDate',DateType::class, [
