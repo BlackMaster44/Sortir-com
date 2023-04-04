ac5374195302df2d3b0c2194d24538adbf896dfb
vivien <spamoremas@gmail.com>
2023-04-04 17:16:38 +0200

    forms

--- src/Form/CreateHangoutType.php
+++ src/Form/CreateHangoutType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Hangout;
 use App\Entity\Place;
+use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -18,14 +20,18 @@ class CreateHangoutType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-
+        $date = new DateTime();
+        $formattedDateTime = $date->format('Y-m-d H:i');
+        $formattedDate = $date->format('Y-m-d');
         $builder
             ->add('name')
             ->add('startTimestamp',DateTimeType::class, [
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'attr'=>['value'=>$formattedDateTime]
             ])
             ->add('lastRegisterDate',DateType::class, [
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'attr'=>['value'=>$formattedDate]
 
             ])
             ->add('maxSlots')
@@ -41,12 +47,22 @@ class CreateHangoutType extends AbstractType
             ->add('place', EntityType::class, [
                 'class' => Place::class,
                 'choice_label' => 'name',
+                'attr'=>['class'=>'label-choice']
             ])
+            ->add('addPlace', ButtonType::class, [
+                'label' => 'Add Place...',
+                'attr'=>['class'=>'button add-place-button']
+            ])
+
             ->add('publish', SubmitType::class, [
-                'attr'=>['class' =>'button']
+                'attr'=>['class' =>'button publish-button button-green']
             ])
             ->add('save', SubmitType::class, [
-                'attr'=>['class'=>'button']
+                'attr'=>['class'=>'button save-button']
+            ])
+            ->add('cancel', ButtonType::class, [
+                'label'=>'Cancel Hangout creation',
+                'attr'=>['class' => 'button cancel-hangout button-red']
             ])
         ;
     }
